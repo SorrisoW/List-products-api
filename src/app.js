a8dfd06b73bc9417964333a20d5eee29f5051f3b
SorrisoW <metranca.w.g@gmail.com>
2025-10-22 18:24:10 -0300

    implementado env

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ import express from "express";
 import helmet from "helmet"; 
 import cors from "cors";
 import rateLimit from "express-rate-limit";
+import { env } from "./config/env.js";
 
 export const createApp = () => {
-    const app = express
+    const app = express()
 
     app.use(express.json())
     app.use(helmet())
-    app.use(cors({ origin: "*" }))
+    app.use(cors({ origin: env.corsOrigin }))
     app.use(rateLimit({ windowMs: 60_000, max: 100}))
 
-    app.length("/health", (_request, response) => {
+    app.get("/health", (_request, response) => {
         response.json({ok: true})
     })
+
+    return app;
 }
